bbd5efe60c82c06d9d5f3534af4febb43709c371
jpg <jphigross@gmail.com>
2025-01-11 07:09:33 -0500

    compute total amount + handle continue shopping nav

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,11 +9,15 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach(element => {
+        totalAmount+=Number(element.cost.replace('$', '')) * element.quantity;
+    });
+    return totalAmount; 
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
@@ -30,6 +34,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+
   };
 
   return (
