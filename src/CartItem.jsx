a63beee0a137ff3535d90a85c90143f820654333
jpg <jphigross@gmail.com>
2025-01-11 07:11:47 -0500

    manage checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -20,6 +20,9 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert ('Functionality to be added for future reference');
+  };
 
 
   const handleIncrement = (item) => {
@@ -62,7 +65,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
